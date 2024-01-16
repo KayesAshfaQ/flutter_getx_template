0654d5fb7f97b58e90216ee9ba3ce09ae507da21
kayesAshfaq <asfak7074@gmail.com>
2024-01-16 18:35:08 +0600

    theme and lang update sample implemented in home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
+import '../../../../config/theme/my_theme.dart';
+import '../../../../config/theme/theme_extensions/header_container_theme_data.dart';
+import '../../../../config/translations/localization_service.dart';
+import '../../../../config/translations/strings_enum.dart';
 import '../../../core/base/base_view.dart';
 import '../controllers/home_controller.dart';
 
@@ -16,10 +23,83 @@ class HomeView extends BaseView<HomeController> {
 
   @override
   Widget body(BuildContext context) {
-    return const Center(
-      child: Text(
-        'HomeView is working',
-        style: TextStyle(fontSize: 20),
+    final theme = Theme.of(context);
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //----------------Theme Button----------------//
+          InkWell(
+            onTap: () => MyTheme.changeTheme(),
+            child: Ink(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 39.h,
+                    width: 39.h,
+                    decoration: theme.extension<HeaderContainerThemeData>()?.decoration,
+                    child: SvgPicture.asset(
+                      Get.isDarkMode ? 'assets/vectors/moon.svg' : 'assets/vectors/sun.svg',
+                      fit: BoxFit.none,
+                      color: Colors.white,
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                  10.horizontalSpace,
+                  Text(
+                    Get.isDarkMode ? 'Dark' : 'Light',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          10.verticalSpace,
+
+          //----------------Language Button----------------//
+          InkWell(
+            onTap: () => LocalizationService.updateLanguage(
+              LocalizationService.getCurrentLocal().languageCode == 'ar' ? 'en' : 'ar',
+            ),
+            child: Ink(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 39.h,
+                    width: 39.h,
+                    decoration: theme.extension<HeaderContainerThemeData>()?.decoration,
+                    child: SvgPicture.asset(
+                      'assets/vectors/language.svg',
+                      fit: BoxFit.none,
+                      color: Colors.white,
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                  10.horizontalSpace,
+                  Text(
+                    LocalizationService.getCurrentLocal().languageCode == 'en' ? 'English' : 'العربية',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          10.verticalSpace,
+
+          Text(
+            '${Strings.goodMorning.tr} ,${Strings.hello.tr}',
+            style: const TextStyle(fontSize: 20),
+          ),
+        ],
       ),
     );
   }
--- lib/config/translations/ar_AR/ar_ar_translation.dart
+++ lib/config/translations/ar_AR/ar_ar_translation.dart
@@ -1,7 +1,8 @@
 import '../strings_enum.dart';
 
-final Map<String, String> arAR =
-{
+final Map<String, String> arAR = {
+  Strings.hello : 'مرحباً!',
+  Strings.loading : 'جاري التحميل',
   
   Strings.changeTheme : 'تغيير الثيم',
   Strings.changeLanguage : 'تغيير اللغة',
